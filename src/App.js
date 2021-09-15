56f95d1e562a79db3053ad783668e139160b6707
Julien Pouillard <julien@heroes.jobs>
2021-09-15 10:27:02 +0200

    Simple interface

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { Container, Row, Col, Input } from "reactstrap"
+import { useState } from "react"
+import axios from "axios"
 
 function App() {
+  const [number, setNumber] = useState(0)
+  const [roman, setRoman] = useState("")
+
+  const handleTranslate = async (evt) => {
+    const int = parseInt(evt.target.value)
+    if (isNaN(int)) return
+    setNumber(int)
+
+    const roman = await translate(int)
+    setRoman(roman)
+  }
+
+  const translate = async (number) => {
+    const response = await axios.get(
+      "http://localhost:4000/translate/roman?arabic=" + number
+    )
+    return response.data.roman
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="root">
+      <Container>
+        <Row className="mt-4">
+          <Col md={5}>
+            <Input type="text" onChange={handleTranslate} value={number} />
+          </Col>
+          <Col md={1} className="text-center">
+            =>
+          </Col>
+          <Col md={5}>
+            <Input type="text" value={roman} readOnly />
+          </Col>
+        </Row>
+      </Container>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
